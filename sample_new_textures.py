a12b1f4e58cf06d375f4fe399c45ef87234c5d75
a.grigorev <a.grigorev@samsung.com>
2021-04-20 22:57:27 +0300

    Readme added

--- sample_new_textures.py
+++ sample_new_textures.py
@@ -11,26 +11,28 @@ from utils.demo import DemoInferer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--textures_root', type=str, default='data/textures')
-    parser.add_argument('--checkpoint_path', type=str, default='data/checkpoints/generative_model.pth')
-    parser.add_argument('--smplx_model_dir', type=str, default='data/smplx')
-    parser.add_argument('--texture_batch_name', type=str)
-    parser.add_argument('--n_samples', type=int, default=10)
-    parser.add_argument('--n_rotimgs', type=int, default=8)
-    parser.add_argument('--imsize', type=int, default=1024)
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--textures_root', type=str, default='data/textures', help='Root deractory to store textures in')
+    parser.add_argument('--checkpoint_path', type=str, default='data/checkpoints/generative_model.pth', help='Path to generative model checkpoint')
+    parser.add_argument('--smplx_model_dir', type=str, default='data/smplx', help='Path to smplx models')
+    parser.add_argument('--texture_batch_name', type=str, help='An identifier for the current run of this script')
+    parser.add_argument('--n_samples', type=int, default=10, help='Number of textures to sample')
+    parser.add_argument('--n_rotimgs', type=int, default=8, help='Number of rotation steps to render textured model in')
+    parser.add_argument('--imsize', type=int, default=1024, help='Resolution in which to render rotation steps')
+    parser.add_argument('--seed', type=int, help='Rendom seed')
+    parser.add_argument('--device', type=str, default='cuda:0', help='Device to run sampling process on')
     args = parser.parse_args()
 
-    set_random_seed(args.seed)
+    if args.seed is not None:
+        set_random_seed(args.seed)
 
     inferer = DemoInferer(args.checkpoint_path, args.smplx_model_dir, imsize=args.imsize, device=args.device)
+    batch_dir = os.path.join(args.textures_root, args.texture_batch_name)
 
     for i in tqdm(range(args.n_samples)):
         with torch.no_grad():
             ntexture = inferer.sample_texture()
 
-        texture_save_dir = os.path.join(args.textures_root, args.texture_batch_name, f"{i:04}")
+        texture_save_dir = os.path.join(batch_dir, f"{i:04}")
         os.makedirs(texture_save_dir, exist_ok=True)
 
         texture_out_path = os.path.join(texture_save_dir, 'texture.pth')
@@ -46,3 +48,5 @@ if __name__ == '__main__':
                 rgb_out_path = os.path.join(texture_save_dir, 'rotation_images', f"{j:04d}.png")
                 os.makedirs(os.path.dirname(rgb_out_path), exist_ok=True)
                 cv2.imwrite(rgb_out_path, rgb[..., ::-1])
+
+    print(f"Stored {args.n_samples} random textures into {batch_dir}")
